5407797626867c5f2f084783f0bab880b4398c83
Ben <bforman@users.noreply.github.com>
2016-03-26 12:44:58 -0400

    added

---
+++ app/registerNode.ts
@@ -0,0 +1,25 @@
+class RegisterNode {
+  public w: string[3];
+  public m: string[3];
+  public e: string[3];
+  public d: string[3];
+  public f: string;
+
+  constructor(public w, public m, public e, public d, public f) {
+    w[0] = "";
+    w[1] = "";
+    w[2] = "";
+    m[0] = "";
+    m[1] = "";
+    m[2] = "";
+    e[0] = "";
+    e[1] = "";
+    e[2] = "";
+    d[0] = "";
+    d[1] = "";
+    d[2] = "";
+    f = "";
+  }
+
+}
+
