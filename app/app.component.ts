8f1d0435ccad030a0cd0be89f5f9e24b422966cd
Zangamj <darknessandshadows@gmail.com>
2016-04-25 14:49:43 -0400

    Modified header, work on adding second linked list and ability to view both

--- app/app.component.ts
+++ app/app.component.ts
@@ -13,34 +13,76 @@ import {ParserService} from "./parser.service";
 export class AppComponent {
   public cn: CycleNode;
   public linkedList: DoublyLinkedList;
+  public correctList: DoublyLinkedList;
 	
 	constructor(private _fileManagerService: FileManagerService,
 				private _parserService: ParserService) {
-		var callback = () : void => {
-			var data: string;
-			this.linkedList = new DoublyLinkedList();
-			while(this._fileManagerService.hasNextClock()) {
-				data = this._fileManagerService.getNextClock();
-				this.linkedList.insert(this._parserService.parse(data));
-			}
-			this.linkedList.reset();
-			this.cn = this.linkedList.getCurrent();
-		};
-		this._fileManagerService.callback = callback;
     }
 	
-	changeListener($event) {
-		if($event.target.files.length != 0)
+	userUploadListener($event) {
+		if($event.target.files.length != 0) {
+			this._fileManagerService.callback = () : void => {
+				var data: string;
+				this.linkedList = new DoublyLinkedList();
+				while(this._fileManagerService.hasNextClock()) {
+					data = this._fileManagerService.getNextClock();
+					this.linkedList.insert(this._parserService.parse(data));
+				}
+				this.resetLists();
+				this.cn = this.linkedList.getCurrent();
+			};
 			this._fileManagerService.readFile($event.target.files[0]);
+		}
+	}
+	
+	correctUploadListener($event) {
+		if($event.target.files.length != 0) {
+			this._fileManagerService.callback = () : void => {
+				var data: string;
+				this.correctList = new DoublyLinkedList();
+				while(this._fileManagerService.hasNextClock()) {
+					data = this._fileManagerService.getNextClock();
+					this.correctList.insert(this._parserService.parse(data));
+				}
+				this.resetLists();
+			};
+			this._fileManagerService.readFile($event.target.files[0]);
+		}
 	}
 
 	nextListener() {
-		this.linkedList.next();
-		this.cn = this.linkedList.getCurrent();
+		if(this.linkedList != null)
+			if(this.linkedList.next())
+				this.cn = this.linkedList.getCurrent();
+		if(this.correctList != null) this.correctList.next();
 	}
 	
 	prevListener() {
-		this.linkedList.previous();
-		this.cn = this.linkedList.getCurrent();
+		if(this.linkedList != null)
+			if(this.linkedList.previous())
+				this.cn = this.linkedList.getCurrent();
+		if(this.correctList != null) this.correctList.previous();
+	}
+	
+	runListener() {
+		if(this.linkedList != null && this.correctList != null) {
+			do {
+				if(!this.compare()) return;
+			} while(this.linkedList.next() && this.correctList.next());
+			this.cn = this.linkedList.getCurrent();
+		}
+	}
+	
+	public resetLists() {
+		if(this.linkedList != null) {
+			this.linkedList.reset();
+			this.cn = this.linkedList.getCurrent();
+		}
+		if(this.correctList != null) this.correctList.reset();
+	}
+	
+	public compare(): boolean {
+		//TODO: Compare the two currents, update view, return true if identical, false if different
+		return true;
 	}
 }
