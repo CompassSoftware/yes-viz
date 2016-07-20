e998efe048896f8a9ca96a4aed9bfc7c845aa101
zangamj <darknessandshadows@gmail.com>
2016-07-20 13:08:38 -0400

    Not sure why src wasn't added

--- gulpfile.js
+++ gulpfile.js
@@ -12,7 +12,7 @@ gulp.task("tscompile", function() {
 
     var tsResult = gulp.src([
         "node_modules/angular2/typings/browser.d.ts",
-	"app/*.ts"
+		"src/*.ts"
     ]).pipe(ts(tsProject));
 
     return tsResult.js.pipe(concat("app.min.js"))
@@ -22,13 +22,13 @@ gulp.task("tscompile", function() {
 gulp.task("getLibs", function() {
     gulp.src([
         "node_modules/es6-shim/es6-shim.min.js",
-	"node_modules/angular2/es6/dev/src/testing/shims_for_IE.js",
-	"node_modules/systemjs/dist/system-polyfills.js",
-	"node_modules/angular2/bundles/angular2-polyfills.js",
-	"node_modules/systemjs/dist/system.src.js",
-	"node_modules/rxjs/bundles/Rx.js",
-	"node_modules/angular2/bundles/angular2.dev.js",
-	"node_modules/angular2/bundles/http.dev.js"
+		"node_modules/angular2/es6/dev/src/testing/shims_for_IE.js",
+		"node_modules/systemjs/dist/system-polyfills.js",
+		"node_modules/angular2/bundles/angular2-polyfills.js",
+		"node_modules/systemjs/dist/system.src.js",
+		"node_modules/rxjs/bundles/Rx.js",
+		"node_modules/angular2/bundles/angular2.dev.js",
+		"node_modules/angular2/bundles/http.dev.js"
     ]).pipe(concat("libs.min.js")).pipe(uglify()).pipe(gulp.dest("build"));
 });
 
@@ -40,8 +40,8 @@ gulp.task("getBoot", function() {
 gulp.task("build", ["tscompile", "getLibs", "getBoot"], function() {
     gulp.src("index.html").pipe(htmlreplace({
         "libraries": "libs.min.js",
-	"app": "app.min.js",
-	"boot": "boot.min.js"
+		"app": "app.min.js",
+		"boot": "boot.min.js"
     })).pipe(gulp.dest("build"));
 
     gulp.src("app/*.{html,css}").pipe(gulp.dest("build/app"));
